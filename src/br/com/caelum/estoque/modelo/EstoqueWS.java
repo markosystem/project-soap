c89592f3f67201fec4d27a288955d4e423d8c8b5
Marcos Batista <markos.system@gmail.com>
2019-10-20 21:48:03 -0300

    Criação do projeto, a apartir do módulo 3 do curso na alura

---
+++ src/br/com/caelum/estoque/modelo/EstoqueWS.java
@@ -0,0 +1,22 @@
+package br.com.caelum.estoque.modelo;
+
+import javax.jws.WebMethod;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+import br.com.caelum.estoque.modelo.item.ItemDao;
+import br.com.caelum.estoque.modelo.item.ListaItens;
+
+@WebService
+public class EstoqueWS {
+
+	private ItemDao dao = new ItemDao();
+
+	@WebMethod(operationName = "todosOsItens")
+	@WebResult(name = "itens")
+	public ListaItens getItens() {
+		System.out.println("Chamando getItens()");
+		return new ListaItens(dao.todosItens());
+	}
+	
+}
